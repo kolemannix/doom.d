99c1bd518d5b09f140af3919ad63664a54155698
Koleman Nix <kolemannix@gmail.com>
2021-11-01 19:11:09 -0400

    dooooom

--- config.el
+++ config.el
@@ -25,7 +25,10 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq! doom-theme 'doom-one)
+;; (load-theme 'doom-homage-white)
+;; (load-theme 'doom-city-lights)
+;; (load-theme 'doom-homage-white)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -52,3 +55,22 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq lsp-ui-doc-enable nil)
+(setq lsp-symbol-highlighting-skip-current t)
+(setq lsp-signature-render-documentation t)
+(setq lsp-signature-auto-activate t)
+
+
+(map! :desc "Navigate codebases using Enter and Delete"
+      :n
+      "<RET>" 'xref-find-definitions
+      )
+(map! :n "<DEL>" 'xref-pop-marker-stack)
+
+(custom-set-faces!
+  ;; Quiet down that highlighting
+  '(lsp-face-highlight-textual :underline nil)
+  )
+
+(setq! lsp-metals-super-method-lenses-enabled t)
--- init.el
+++ init.el
@@ -107,7 +107,7 @@
 
        :os
        (:if IS-MAC macos)  ; improve compatibility with macOS
-       ;;tty               ; improve the terminal Emacs experience
+       tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
@@ -159,7 +159,7 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        (scala +lsp)             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
